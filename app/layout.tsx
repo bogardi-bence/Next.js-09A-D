23bfba70f8e7dda1d29a967904ff4875ae5dde97
danieladaam <daniel.adam@students.jedlik.eu>
2026-05-11 12:25:51 +0200

    óravége

---
+++ app/layout.tsx
@@ -0,0 +1,11 @@
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="hu">
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function KörmozgásPage() {
+  return (
+    <div>
+      <div className="flex items-center justify-center flex-col min-h-screen">
+        <div className="flex w-80 flex-col items-center ">
+          <h1>KörmozgásPage</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
