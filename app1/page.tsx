065e7481f0af349cf04fe5381f79a190455773c5
bogardi-bence <bogardi.bence@students.jedlik.eu>
2026-05-13 09:56:10 +0200

    oldal összevonása

--- app1/page.tsx
+++ app1/page.tsx
@@ -1,5 +1,16 @@
-<<<<<<< HEAD
-export default function KörmozgásPage() {
+type SearchParams = {
+    r?: string;
+    T?: string;
+}
+
+export default function KeruletiSebesseg({ searchParams }) {
+  
+  const K = searchParams;
+  const r: number = Number(K.r) || 5;
+  const T: number = Number(K.T) || 5;   
+
+  const keruletisebesseg = 2 * Math.PI * r / T;
+  
   return (
     <div>
       <div className="flex items-center justify-center flex-col min-h-screen">
@@ -10,31 +21,7 @@ export default function KörmozgásPage() {
     </div>
   )
 }
-=======
-type SearchParams = {
-    r?: string;
-    T?: string;
-}
-
-
-export default function KeruletiSebesseg({ searchParams }) {
-  
-  const K = searchParams;
-  const r: number = Number(K.r) || 5;
-  const T: number = Number(K.T) || 5;   
-
-  const keruletisebesseg = 2 * Math.PI * r / T;
-  
-    return (
-    <div>KeruletiSebesseg</div>
-    <form>
-    <div>
-        <label className=""></label>
-    </div>
-    </form>
-  )
-}
 
 
 
->>>>>>> e1a298549a05ae32340ce01403cef10e7cc262c0
+
