3a7bd371b157972300d102ee57c2027380f704f3
danieladaam <daniel.adam@students.jedlik.eu>
2026-05-17 19:57:22 +0200

    design

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,35 @@
-type SearchParams = {
-  r?: string;
-  T?: string;
+type LinkType = {
+  href: string;
+  label: string;
 };
+const links: LinkType[] = [
+  { href: "/keruletisebesseg", label: "Kerületi sebesség" },
+  { href: "/abrazolas", label: "Ábrázolás" },
 
-export default async function KeruletiSebesseg({ searchParams }: { searchParams: SearchParams }) {
-  const K: SearchParams = await searchParams;
-
-  let r: number = Number(K.r) || 5;
-  let T: number = Number(K.T) || 5;
-
-  r = Math.floor(r) !== r ? 5 : r;
-  T = Math.floor(T) !== T ? 5 : T;
-
-  const keruletisebesseg = (2 * Math.PI * r) / T;
-
+];
+import Link from "next/link";
+export default function KeruletiSebesseg() {
   return (
-    <div className="bg-red-500 flex min-h-screen flex-col items-left justify-left">
-      <div className="w-150 rounded-xl bg-white p-5 font-mono shadow-xl">
-        <h1 className="text-2xl font-semibold mb-5 text-center">Kerületi sebesség</h1>
-        <form className="space-y-2">
-          <div>
-            <label className="mr-4">r =</label>
-            <input className="input input-primary" defaultValue={r} name="r" type="text" />
-          </div>
-          <div>
-            <label className="mr-4">T =</label>
-            <input className="input input-primary" defaultValue={T} name="T" type="text" />
-          </div>
-          <div className="mt-4">
-            <p className="text-center text-2xl font-semibold">
-              Kerületi sebesség = {keruletisebesseg.toFixed(2)}
-            </p>
-          </div>
-          <button className="hidden" type="submit"></button>
-        </form>
+    <div
+      className="flex min-h-screen flex-col items-center justify-center"
+      style={{ background: "linear-gradient(rgba(8, 62, 209, 1), rgba(67, 109, 224, 1), white)" }}
+    >
+      <div
+        className="w-150 rounded-xl p-5 font-mono shadow-xl"
+        style={{ color: "white", backgroundColor: "rgba(41, 39, 110, 1)" }}
+      >
+        <h1 className="mb-5 text-center text-2xl font-semibold">Kerületi sebesség</h1>
+      <ul className="mt-3 items-center justify-center flex flex-col gap-3">
+          {links.map((link, index) => (
+            <li key={index}>
+              <Link className=""  href={link.href}>
+                {link.label}
+              </Link>
+            </li>
+          ))}
+        </ul>  
+        
       </div>
     </div>
   );
-}
+};
